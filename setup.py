2bb0e1e594cf0389a9c8f02ec257566bffbbacc3
Aarish-PC <aarishalam22@gmail.com>
2024-07-16 10:24:47 +0530

    added env and folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa_Approval",
+    version="0.0.0",
+    author="Aarish",
+    author_email="aarishalam22@gmail.com",
+    packages=find_packages()
+)
